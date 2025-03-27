f695d46a44e3afb7470bb834f7bdcc0ecef7f22c
AFCMS <afcm.contact@gmail.com>
2025-03-27 12:11:12 +0100

    Update everything

--- gen_protobuf.sh
+++ gen_protobuf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 generated_outputs="./moteur_jeu ./serveur_http ./serveur_tcp"
 for path in $generated_outputs; do
-    python -m grpc_tools.protoc -I./moteur_jeu/protos --python_out=$path/generated --pyi_out=$path/generated --grpc_python_out=$path/generated ./moteur_jeu/protos/server.proto
+    python -m grpc_tools.protoc -I./moteur_jeu/protos --python_out=$path --pyi_out=$path --grpc_python_out=$path ./moteur_jeu/protos/server.proto
 done
