4f0d02d9bf5d94fd66622e93c51fc31690b26430
Hue <emilien.dessarps@gmail.com>
2025-03-31 21:31:06 +0200

    Mostly working state Triggers, procedures and functions should all be good Still lacks a view

--- db.d/src/wv_triggers.sql
+++ db.d/src/wv_triggers.sql
@@ -1,4 +1,28 @@
-CREATE TRIGGER enforce_lowercase_name
+-- triggers need functions, not procedures, so gotta wrap it up like a christmas present on… christmas day. Cmon, i can’t always be funny, it’s hard
+CREATE FUNCTION username_to_lower_function()
+RETURNS TRIGGER 
+LANGUAGE plpgsql AS $$
+BEGIN
+    CALL username_to_lower();
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER enforce_username_to_lower
 AFTER INSERT ON players
 FOR EACH ROW
-EXECUTE PROCEDURE USERNAME_TO_LOWER();
+EXECUTE FUNCTION username_to_lower_function();
+
+CREATE FUNCTION complete_turn_function()
+RETURNS TRIGGER
+LANGUAGE plpgsql AS $$
+BEGIN
+	IF NOT EXISTS (SELECT id_player FROM players_in_parties WHERE id_party = NEW.id_PARTY AND is_alive = TRUE) EXCEPT (SELECT id_player FROM players_play WHERE id_turn = NEW.id_turn)
+	THEN CALL COMPLETE_TOUR(NEW.id_turn, NEW.id_party);
+	RETURN NEW;
+END;
+
+CREATE TRIGGER enforce_complete_tour
+AFTER INSERT ON players_play
+FOR EACH ROW
+EXECUTE FUNCTION complete_tour_function();
