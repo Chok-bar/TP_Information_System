be63437a79433331a6af5ae1519a4f4d49e175f2
THOMAS FOUQUET <thomas.fouquet@ecole-hexagone.com>
2025-03-31 11:47:40 +0200

    MAJ pour le moteur de jeu

--- gen_protobuf.sh
+++ gen_protobuf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 generated_outputs="./moteur_jeu ./serveur_http ./serveur_tcp"
 for path in $generated_outputs; do
-    python -m grpc_tools.protoc -I./moteur_jeu/protos --python_out=$path --pyi_out=$path --grpc_python_out=$path ./moteur_jeu/protos/server.proto
+    python -m grpc_tools.protoc -I./moteur_jeu --python_out=$path --pyi_out=$path --grpc_python_out=$path ./moteur_jeu/server.proto
 done
